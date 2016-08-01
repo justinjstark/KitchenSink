5c32b079329df61c95bb1aeb6e72bd6665a93ca0
justinjstark <fromgithub@justinjstark.com>
2016-08-01 16:54:02 -0500

    Add basic MVC app

---
+++ src/KitchenSink.Tests/Class1.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using Shouldly;
+
+namespace KitchenSink.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Test1()
+        {
+            true.ShouldBe(true);
+        }
+    }
+}
---
+++ src/KitchenSink.Web/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KitchenSink.Web.Startup))]
+namespace KitchenSink.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
