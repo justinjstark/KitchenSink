b0c7a3122b730760c91e9fb7f185e9cec057f43d
Justin J Stark <fromgithub@justinjstark.com>
2016-08-22 21:33:30 -0500

    Add GitVersiona nd ReleaseNotesHelper to build

--- build.fsx
+++ build.fsx
@@ -1,51 +1,96 @@
 // include Fake lib
 #r @"build/packages/FAKE/tools/FakeLib.dll"
+#r @"build/packages/Newtonsoft.Json/lib/portable-net45+wp80+win8+wpa81/Newtonsoft.Json.dll"
 
 open Fake
 open Fake.Testing.NUnit3
 open Fake.NuGet.Install
 open Fake.AssemblyInfoFile
+open Newtonsoft.Json
 
 //RestorePackages()
 
 // Directories
-let buildDir  = @"./build/"
-let testDir   = @"./test/"
-let deployDir = @"./deploy/"
+let outputDir  = @"./build/output/"
+let testDir   = @"./build/test/"
+let deployDir = @"./build/deploy/"
 let packagesDir = @"./build/packages"
 
 // tools
-let fxCopRoot = @"./Tools/FxCop/FxCopCmd.exe"
+let fxCopExe = @"./Tools/FxCop/FxCopCmd.exe"
+
+let gitVersionExe = findToolInSubPath "GitVersion.exe" (packagesDir @@ "GitVersion.CommandLine")  
+
+let release =
+    ReadFile "RELEASE_NOTES.md"
+    |> ReleaseNotesHelper.parseReleaseNotes
 
 // version info
-let version = "0.0.1"  // Use GitVersion to get this
+let releaseNotesVersion = release.AssemblyVersion
+log ("ReleaseNotesVersion: " + releaseNotesVersion)
+
+//let version2 = GitVersionHelper.GitVersion (fun p ->
+//    { p with ToolPath = findToolInSubPath "GitVersion.exe" (packagesDir @@ "GitVersion.CommandLine") } //packagesDir @@ "/GitVersion.CommandLine/tools/GitVersion.exe"}
+//)
+//log ("GitVersion: " + version2)
+
+//Workaround https://github.com/fsharp/FAKE/pull/1365
+type GitVersionProperties = {
+    Major : int;
+    Minor : int;
+    Patch : int;
+    PreReleaseTag : string;
+    PreReleaseTagWithDash : string;
+    PreReleaseLabel : string;
+    PreReleaseNumber : System.Nullable<int>;
+    BuildMetaData : string;
+    BuildMetaDataPadded : string;
+    FullBuildMetaData : string;
+    MajorMinorPatch : string;
+    SemVer : string;
+    LegacySemVer : string;
+    LegacySemVerPadded : string;
+    AssemblySemVer : string;
+    FullSemVer : string;
+    InformationalVersion : string;
+    BranchName : string;
+    Sha : string;
+    NuGetVersionV2 : string;
+    NuGetVersion : string;
+    CommitsSinceVersionSource : int;
+    CommitsSinceVersionSourcePadded : string;
+    CommitDate : string;
+}
+
+let myGitVersionHelper = 
+    let timespan =  System.TimeSpan.FromMinutes 1.
+    let gitVersionToolPath = gitVersionExe
+    let result = ExecProcessAndReturnMessages (fun info ->
+        info.FileName <- gitVersionToolPath) timespan
+    if result.ExitCode <> 0 then failwithf "GitVersion.exe failed with exit code %i" result.ExitCode
+    result.Messages |> String.concat "" |> fun j -> JsonConvert.DeserializeObject<GitVersionProperties>(j)
+
+let gitVersion = myGitVersionHelper
+log ("GitVersion: " + gitVersion.SemVer)
 
 // Targets
 Target "Clean" (fun _ ->
-    CleanDirs [buildDir; testDir; deployDir]
+    CleanDirs [outputDir; testDir; deployDir]
 )
 
 Target "SetVersions" (fun _ ->
     CreateCSharpAssemblyInfo "./src/KitchenSink.Web/Properties/AssemblyInfo.cs"
-        [//Attribute.Title "Kitchen Sink Continuous Delivery"
-         //Attribute.Description "An example of continous delivery with all the bells and whistles"
-         //Attribute.Guid "A539B42C-CB9F-4a23-8E57-AF4E7CEE5BAA"
-         //Attribute.Product "Calculator"
-         Attribute.Version version
-         Attribute.FileVersion version]
+        [Attribute.Version gitVersion.AssemblySemVer
+         Attribute.FileVersion gitVersion.AssemblySemVer]
 
     CreateCSharpAssemblyInfo "./src/KitchenSink.Web.Tests/Properties/AssemblyInfo.cs"
-        [//Attribute.Title "Calculator library"
-         //Attribute.Description "Sample project for FAKE - F# MAKE"
-         //Attribute.Guid "EE5621DB-B86B-44eb-987F-9C94BCC98441"
-         //Attribute.Product "Calculator"
-         Attribute.Version version
-         Attribute.FileVersion version]
+        [Attribute.Version gitVersion.AssemblySemVer
+         Attribute.FileVersion gitVersion.AssemblySemVer]
 )
 
 Target "CompileApp" (fun _ ->
     !! @"src/**/*.csproj"
-      |> MSBuildRelease buildDir "Build"
+      |> MSBuildRelease outputDir "Build"
       |> Log "AppBuild-Output: "
 )
 
@@ -56,7 +101,7 @@ Target "CompileTests" (fun _ ->
 )
 
 Target "RunTests" (fun _ ->
-    !! (testDir + @"/NUnit.Test.*.dll")
+    !! (testDir @@ @"/**/**.Tests.dll")
       |> NUnit (fun p ->
                  {p with
                    DisableShadowCopy = true;
@@ -64,28 +109,31 @@ Target "RunTests" (fun _ ->
 )
 
 Target "FxCop" (fun _ ->
-    !! (buildDir + @"/**/*.dll")
-      ++ (buildDir + @"/**/*.exe")
+    !! (outputDir @@ @"/**/*.dll")
+      ++ (outputDir + @"/**/*.exe")
         |> FxCop (fun p ->
             {p with
                 ReportFileName = testDir + "FXCopResults.xml";
-                ToolPath = fxCopRoot})
+                ToolPath = fxCopExe})
 )
 
-Target "Zip" (fun _ ->
-    !! (buildDir + "/**/*.*")
-        -- "*.zip"
-        |> Zip buildDir (deployDir + "Calculator." + version + ".zip")
+Target "Release" (fun _ ->
+    if gitVersion.BuildMetaData = "1" then log "~~~ !!! RELEASE TIME !!! ~~~"
+    //Set an env variable to let build server know to deploy !??
 )
 
 // Dependencies
 "Clean"
   ==> "SetVersions"
   ==> "CompileApp"
-  ==> "FxCop"
+  //==> "FxCop"
   ==> "CompileTests"
   ==> "RunTests"
-  ==> "Zip"
+  ==> "Release"
 
 // start build
 RunTargetOrDefault "SetVersions"
+
+//match buildServer with
+//    | AppVeyor -> "test"
+//    | _ -> ""
