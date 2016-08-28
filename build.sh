221428172999d614c3711c33f80108c3611f2257
Justin J Stark <fromgithub@justinjstark.com>
2016-08-28 16:49:16 -0500

    Fix build scripts

--- build.sh
+++ build.sh
@@ -3,5 +3,4 @@
 export EnableNuGetPackageRestore=true
 mono .nuget/nuget.exe Install FAKE -OutputDirectory ./build/packages -ExcludeVersion
 mono .nuget/nuget.exe Install GitVersion.CommandLine -OutputDirectory ./build/packages -ExcludeVersion
-mono .nuget/nuget.exe Install Newtonsoft.Json -OutputDirectory ./build/packages -ExcludeVersion
 mono build/packages/FAKE/tools/Fake.exe build.fsx "$@"
